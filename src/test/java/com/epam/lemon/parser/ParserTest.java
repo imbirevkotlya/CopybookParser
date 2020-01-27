663639b6701cc79b8d07a21af92b7bab9ea21953
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-01-27 14:34:46 +0300

    add new comp test cases with test copybooks

--- src/test/java/com/epam/lemon/parser/ParserTest.java
+++ src/test/java/com/epam/lemon/parser/ParserTest.java
@@ -58,4 +58,41 @@ public class ParserTest {
 
         new CopybookParser().parse(copybookStatementIterator);
     }
+
+    @Test
+    public void parseCopybookWithCompFields() throws IOException {
+        CopybookStatementIterator copybookStatementIterator = new CopybookStatementIterator(Files.readAllBytes(Path.of("src/test/resources/COMP-COPYBOOK.cpy")));
+
+        Copybook copybook = new CopybookParser().parse(copybookStatementIterator);
+
+        List<DataDeclarationCobolStatement> actualStatements = copybook.getCobolStatements();
+        Assert.assertEquals(1, actualStatements.size());
+        DataDeclarationCobolStatement actualStatement = actualStatements.get(0);
+        Assert.assertEquals(StatementType.GROUP_STATEMENT, actualStatement.getStatementType());
+        Assert.assertEquals(1, actualStatement.getLevel().intValue());
+        Assert.assertEquals("EMP-RECORD", actualStatement.getName());
+        GroupDataDeclarationCobolStatement groupActualStatement = (GroupDataDeclarationCobolStatement) actualStatement;
+        Assert.assertEquals(4, groupActualStatement.getChildrenStatements().size());
+        List<DataDeclarationCobolStatement> actualStatementsWithFiveLevel = copybook.getStatementsByLevel(5);
+        Assert.assertEquals(4, actualStatementsWithFiveLevel.size());
+        Assert.assertEquals(StatementType.INTEGER_STATEMENT, copybook.getStatementByName("EMP-NAME").getStatementType());
+        Assert.assertEquals(StatementType.ALPHANUMERIC_STATEMENT, copybook.getStatementByName("EMP-SURNAME").getStatementType());
+        Assert.assertEquals(1, ((RegularDataDeclarationCobolStatement) copybook.getStatementByName("EMP-POSITION")).getLength().intValue());
+        Assert.assertEquals(StatementType.COMP_STATEMENT, copybook.getStatementByName("EMP-SALARY").getStatementType());
+        Assert.assertEquals(2, ((RegularDataDeclarationCobolStatement) copybook.getStatementByName("EMP-SALARY")).getLength().intValue());
+    }
+
+    @Test(expected = InvalidStatementFormatException.class)
+    public void parse_invalidCompDeclaration() throws IOException {
+        CopybookStatementIterator copybookStatementIterator = new CopybookStatementIterator(Files.readAllBytes(Path.of("src/test/resources/COMP-INVALID-DECLAR.cpy")));
+
+        new CopybookParser().parse(copybookStatementIterator);
+    }
+
+    @Test(expected = InvalidStatementFormatException.class)
+    public void parse_invalidCompDataFormat() throws IOException {
+        CopybookStatementIterator copybookStatementIterator = new CopybookStatementIterator(Files.readAllBytes(Path.of("src/test/resources/COMP-INVALID-FORMAT.cpy")));
+
+        new CopybookParser().parse(copybookStatementIterator);
+    }
 }
---
+++ src/test/resources/COMP-COPYBOOK.cpy
@@ -0,0 +1,5 @@
+       01 EMP-RECORD.
+           05 EMP-NAME PIC 99.
+           05 EMP-SURNAME PIC XX.
+           05 EMP-POSITION PIC X.
+           05 EMP-SALARY PIC 99 COMP.
