f1311cc97742ae0dff8dc22ee786d38b42e0a0b5
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-01-30 15:17:08 +0300

    add interface method with javadoc in the regular fields abstraction

--- src/main/java/com/epam/lemon/statement/RegularDataDeclarationCobolStatement.java
+++ src/main/java/com/epam/lemon/statement/RegularDataDeclarationCobolStatement.java
@@ -11,4 +11,26 @@ public interface RegularDataDeclarationCobolStatement extends DataDeclarationCob
      */
     Integer getLength();
 
+    /**
+     * Method to return the field default value, if it was specified.
+     * Example of pattern to specify:
+     *
+     * 01 NAME PIC 99 VALUE 12.
+     *
+     * The value will be returned is 12.
+     *
+     * If no default value was specified - the statement type default value will be returned:
+     * Alphanumeric - " "
+     * Integer - 0
+     * Computational - 0
+     *
+     * Example:
+     * 01 NAME PIC 99.
+     *
+     * The value will be returned: 00.
+     *
+     * @return the field default value
+     */
+    String getDefaultValue();
+
 }
