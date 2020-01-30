2a723be3c145b4199efa73991752dc626ea72cfb
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-01-30 18:39:37 +0300

    little utility class renaming

--- src/main/java/com/epam/lemon/parser/statement/ArrayMerger.java
+++ src/main/java/com/epam/lemon/parser/statement/ArrayMerger.java
@@ -20,9 +20,10 @@ class ArrayMerger {
      * @return the new array with new length
      */
     String[] merge() {
-        int allStatementAttributeFormatsLength = secondArray.length + firstArray.length;
-        String[] allStatementAttributeFormats = Arrays.copyOf(firstArray, allStatementAttributeFormatsLength);
-        System.arraycopy(secondArray, 0, allStatementAttributeFormats, firstArray.length, secondArray.length);
-        return allStatementAttributeFormats;
+        int fullLength = secondArray.length + firstArray.length;
+        String[] fullArray = Arrays.copyOf(firstArray, fullLength);
+        int initialPosition = 0;
+        System.arraycopy(secondArray, initialPosition, fullArray, firstArray.length, secondArray.length);
+        return fullArray;
     }
 }
