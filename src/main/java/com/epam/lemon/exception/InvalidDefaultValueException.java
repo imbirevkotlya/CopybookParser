a643c14f2081971947111a93cfbd48883fd6256b
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-01-30 18:37:01 +0300

    add exception javadoc

--- src/main/java/com/epam/lemon/exception/InvalidDefaultValueException.java
+++ src/main/java/com/epam/lemon/exception/InvalidDefaultValueException.java
@@ -1,4 +1,7 @@
 package com.epam.lemon.exception;
 
+/**
+ * General exception to indicate that something went wrong with default value in the statement declaration
+ */
 public class InvalidDefaultValueException extends RuntimeException {
 }
