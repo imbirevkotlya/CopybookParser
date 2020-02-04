e1e4af6bfd4308677df9c7a3c132ee01f5fa9d87
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-02-04 17:02:32 +0300

    change the group statement parser and domain entity to support the nested groups

--- src/main/java/com/epam/lemon/parser/statement/group/GroupStatementParser.java
+++ src/main/java/com/epam/lemon/parser/statement/group/GroupStatementParser.java
@@ -1,10 +1,14 @@
 package com.epam.lemon.parser.statement.group;
 
-import com.epam.lemon.parser.statement.AbstractStatementParser;
+import com.epam.lemon.copybook.StatementIterator;
+import com.epam.lemon.exception.InvalidStatementFormatException;
+import com.epam.lemon.parser.statement.StatementParser;
+import com.epam.lemon.parser.statement.registry.StatementParserRegistry;
 import com.epam.lemon.statement.DataDeclarationCobolStatement;
 import com.epam.lemon.statement.group.GroupDataDeclarationCobolStatement;
 
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * COBOL grouping statement contains only two attributes: level and name.
@@ -14,10 +18,22 @@ import java.util.function.Function;
  * 01 GROUP-NAME.
  *
  */
-public class GroupStatementParser extends AbstractStatementParser {
+public class GroupStatementParser extends ParentFieldParser {
 
     private static final int LEVEL = 0;
     private static final int NAME = 1;
+
+    private final StatementParserRegistry statementParserRegistry;
+    /**
+     * Main constructor to initialize object with statement iterator.
+     *
+     * @param statementIterator the statement source to fetch upcoming field declaration
+     */
+    public GroupStatementParser(StatementIterator statementIterator,
+        StatementParserRegistry statementParserRegistry) {
+        super(statementIterator);
+        this.statementParserRegistry = statementParserRegistry;
+    }
 
     /**
      * {@inheritDoc}
@@ -30,14 +46,43 @@ public class GroupStatementParser extends AbstractStatementParser {
         return groupAttributeFormats;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    protected Function<String[], DataDeclarationCobolStatement> getBuildStatementFunction() {
-        return statementAttributes -> new GroupDataDeclarationCobolStatement(
-                Integer.parseInt(statementAttributes[LEVEL]),
-                statementAttributes[NAME]
-        );
+    protected DataDeclarationCobolStatement parseMatchedStatement(String[] groupStatementAttributes) {
+        Integer groupLevel = parseLevel(groupStatementAttributes[LEVEL]);
+        List<DataDeclarationCobolStatement> childrenStatements = parseChildrenStatements(groupLevel);
+        return new GroupDataDeclarationCobolStatement(groupLevel, groupStatementAttributes[NAME], childrenStatements);
     }
+
+    private Integer parseLevel(String statementAttribute) {
+        return Integer.parseInt(statementAttribute);
+    }
+
+    private List<DataDeclarationCobolStatement> parseChildrenStatements(Integer groupLevel) {
+        List<DataDeclarationCobolStatement> childrenStatements = new ArrayList<>();
+        List<StatementParser> statementParsers = statementParserRegistry.registerStatementParsers(
+            statementParserRegistry, statementIterator);
+        while (statementIterator.hasNext()) {
+            String copybookStatement = statementIterator.next();
+            String[] statementAttributes = copybookStatement.split(SPACE);
+            if (parseLevel(statementAttributes[LEVEL]) > groupLevel) {
+                DataDeclarationCobolStatement childStatement = null;
+                for (StatementParser childParser : statementParsers) {
+                    childStatement = childParser.parseStatement(copybookStatement);
+                    if (childStatement != null) {
+                        childrenStatements.add(childStatement);
+                        break;
+                    }
+                }
+                if (childStatement == null) {
+                    throw new InvalidStatementFormatException(copybookStatement);
+                }
+            }
+            else {
+                statementIterator.previous();
+                break;
+            }
+        }
+        return childrenStatements;
+    }
+
 }
