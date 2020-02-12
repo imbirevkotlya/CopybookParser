54109da6f4b6fb85e330a05199e591db1f2ab114
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-02-12 14:53:25 +0300

    add copybook flat structure get ability with unit api tests

---
+++ src/test/java/com/epam/lemon/copybook/CopybookTest.java
@@ -0,0 +1,91 @@
+package com.epam.lemon.copybook;
+
+import com.epam.lemon.statement.DataDeclarationCobolStatement;
+import com.epam.lemon.statement.RegularDataDeclarationCobolStatement;
+import com.epam.lemon.statement.alphanumeric.AlphanumericDeclarationCobolStatement;
+import com.epam.lemon.statement.group.GroupDataDeclarationCobolStatement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CopybookTest {
+
+  private static final String GROUP_NAME = "TEST";
+  private static final String SUBGROUP_NAME = "TEST2";
+  private static final String CHILD_NAME = "CHILD";
+
+  private static final int GROUP_LEVEL = 1;
+  private static final int SUBGROUP_LEVEL = 5;
+  private static final int CHILD_LEVEL = 10;
+
+  private static final int LENGTH = 5;
+
+  private Copybook copybook;
+
+  @Test
+  public void getFlatStructure() {
+    List<DataDeclarationCobolStatement> children = getChildren();
+    copybook = new Copybook(Collections
+        .singletonList(new GroupDataDeclarationCobolStatement(GROUP_LEVEL, GROUP_NAME, children)));
+
+    List<RegularDataDeclarationCobolStatement> actualStatements = copybook
+        .getValueContainsCobolStatements();
+
+    Assert.assertEquals(children, actualStatements);
+  }
+
+  private List<DataDeclarationCobolStatement> getChildren() {
+    List<DataDeclarationCobolStatement> children = new ArrayList<>();
+    children.add(new AlphanumericDeclarationCobolStatement(CHILD_LEVEL, LENGTH, CHILD_NAME));
+    return children;
+  }
+
+  @Test
+  public void getFlatStructure_nestedGroups() {
+    List<DataDeclarationCobolStatement> firstLevelChildren = new ArrayList<>();
+    List<DataDeclarationCobolStatement> secondLevelChildren = getChildren();
+    firstLevelChildren.add(new GroupDataDeclarationCobolStatement(SUBGROUP_LEVEL, SUBGROUP_NAME, secondLevelChildren));
+    copybook = new Copybook(Collections
+        .singletonList(new GroupDataDeclarationCobolStatement(GROUP_LEVEL, GROUP_NAME, firstLevelChildren)));
+
+    List<RegularDataDeclarationCobolStatement> actualStatements = copybook.getValueContainsCobolStatements();
+
+    Assert.assertEquals(secondLevelChildren, actualStatements);
+  }
+
+  @Test
+  public void getFlatStructure_severalGroups() {
+    List<DataDeclarationCobolStatement> secondLevelChildren = getChildren();
+    List<DataDeclarationCobolStatement> firstLevelChildren = new ArrayList<>();
+    firstLevelChildren.add(new GroupDataDeclarationCobolStatement(SUBGROUP_LEVEL, SUBGROUP_NAME, secondLevelChildren));
+    firstLevelChildren.add(new GroupDataDeclarationCobolStatement(SUBGROUP_LEVEL, SUBGROUP_NAME, secondLevelChildren));
+    copybook = new Copybook(Collections
+        .singletonList(new GroupDataDeclarationCobolStatement(GROUP_LEVEL, GROUP_NAME, firstLevelChildren)));
+
+    List<RegularDataDeclarationCobolStatement> actualStatements = copybook.getValueContainsCobolStatements();
+
+    List<DataDeclarationCobolStatement> expected = new ArrayList<>(secondLevelChildren);
+    expected.addAll(secondLevelChildren);
+    Assert.assertEquals(expected, actualStatements);
+  }
+
+  @Test
+  public void getFlatStructure_emptyStatements() {
+    Copybook emptyCopybook = new Copybook(Collections.emptyList());
+
+    List<RegularDataDeclarationCobolStatement> actualStatements = emptyCopybook.getValueContainsCobolStatements();
+
+    Assert.assertEquals(0, actualStatements.size());
+  }
+
+  @Test
+  public void getFlaStructure_onlyValueContainsStatements() {
+    List<DataDeclarationCobolStatement> valueContainsStatements = getChildren();
+
+    copybook = new Copybook(valueContainsStatements);
+
+    Assert.assertEquals(valueContainsStatements, copybook.getValueContainsCobolStatements());
+  }
+}
